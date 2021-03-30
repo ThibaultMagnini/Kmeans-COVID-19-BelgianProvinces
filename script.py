c5d627263fd6aa1174c23f6ae53815849b0b8148
Thibault Magnini <thibault.magnini@student.ucll.be>
2021-03-30 11:36:05 +0200

    Box Plot

--- script.py
+++ script.py
@@ -29,13 +29,15 @@ plt.show() """
 kmeans = {}
 for i in range(1, 6):
     kmeans[i] = Kmeans('Datasets/output.csv', i)
-    kmeans[i].read_csv(['TOTAL_INFECTIONS', 'TEST_ALL'])
+    kmeans[i].read_csv(['INFECTION_RATE', 'ICU_RATE','UNEMPLOYEMENT_RATE', 'TEST_POS_PERCENTAGE'])
     print(kmeans[i].original_dataset)
     kmeans[i].start_clustering()
 
 
 x = []
 y = []
+
+"""
 for key in kmeans:
     plt.subplot(3, 2, key)
     plt.subplots_adjust(hspace=0.35)
@@ -46,22 +48,25 @@ for key in kmeans:
 plt.subplot(3, 2, 6)
 plt.plot(x, y)
 plt.scatter(x, y)
-plt.title("Elbow graph")
+plt.title("Elbow graph")"""
 
 with open("Results/index.html", "w") as file:
     file.write("")
 
 for key in kmeans:
     data = []
+    # draw_boxplot(kmeans[key].original_dataset)
     for i in range(len(kmeans[key].c)):   
         row = []
         row.append(look_for_province(kmeans[key].original_dataset[i]))
         row.append(round(kmeans[key].c[i]))
         row.append(kmeans[key].original_dataset[i][0])
         row.append(kmeans[key].original_dataset[i][1])
+        row.append(kmeans[key].original_dataset[i][2])
+        row.append(kmeans[key].original_dataset[i][3])
         data.append(row)
 
-    table = pd.DataFrame(data, columns = ["Province", "Cluster", "Number of infections", "Number of tests"])
+    table = pd.DataFrame(data, columns = ["Province", "Cluster", "Infection rate %", "ICU rate %", "Unemployment rate %", "Positive test rate %"])
     table = table.sort_values(by=['Cluster'])
     html = generate_html_table(table)
     
--- util.py
+++ util.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pretty_html_table import build_table
+import matplotlib.pyplot as plt
 
 def look_for_province(list):
     data = pd.read_csv('Datasets\output.csv')
     for entry in data.itertuples():
-        if entry[9] == list[0]:
+        if entry[2] == list[0]:
             return entry[1]
     return 
 
@@ -12,3 +13,22 @@ def look_for_province(list):
 def generate_html_table(dataframe):
     table_html = build_table(dataframe, 'blue_light')
     return table_html
+
+
+def draw_boxplot(data):
+    result = []
+    for j in range(len(data[0])):
+        temp = []
+        for i in range(len(data)):
+            temp.append(data[i][j])
+        result.append(temp)
+
+    labels = ["Infection rate %", "ICU rate %", "Unemployment rate %", "Positive test rate %"]
+    plot = plt.boxplot(result, sym='yD', vert=True, patch_artist=True, labels=labels)
+    colors = ["red", "green", "yellow", "purple", "cyan", "blue"]
+    for patch, color in zip(plot['boxes'], colors):
+        patch.set_facecolor(color)
+    plt.title("Boxplot representation of the used parameters")
+    plt.show()
+
+    return
